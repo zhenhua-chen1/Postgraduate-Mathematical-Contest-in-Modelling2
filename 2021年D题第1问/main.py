473d822b6b1a8c07bae95fbdf07a893be292d745
zhenhua-chen1 <41711519+zhenhua-chen1@users.noreply.github.com>
2022-09-15 14:57:22 +0800

    2021年D题第一问

--- 2021年D题第1问/main.py
+++ 2021年D题第1问/main.py
@@ -63,21 +63,51 @@ for n in range(1,n_variable):
 data1_training.drop(data1_training.columns[num_len],axis = 1,inplace = True) #axis参数默认为0 
 print('共剔除'+str(len(num_len))+'个变量')
 
-
-
+#使用随机森林降维
 print('使用随机森林开始降维')
 model = RandomForestRegressor(random_state=1, max_depth=10)
 data1_training=pd.get_dummies(data1_training)
 model.fit(data1_training,data2_output)
 features = data1_training.columns
 importances = model.feature_importances_
-indices = np.argsort(importances[0:19])  # top 20 features
+indices = np.argsort(importances[0:30])  # top 20 features
+plt.figure(1)
 plt.title('Feature Importances')
 plt.barh(range(len(indices)), importances[indices], color='b', align='center')
 plt.yticks(range(len(indices)), [features[i] for i in reversed(indices)])
 plt.xlabel('Relative Importance')
+plt.figure(2)
+plt.rcParams['font.family'] = ['Arial Unicode MS'] #正常显示中文
+plt.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
+plt.plot([importances[i] for i in reversed(indices)])
+plt.title('特征值')
 plt.show()
-print('前20个对生物活性最具有显著影响的分子描述符为:')
-print(range(len(indices)),[features[i] for i in indices])
+print('降维后对生物活性最具有显著影响的30个分子描述符为:')
+print([features[i] for i in indices])
+
+#计算相关性
+print('使用pearson计算30个维度的相关性并输出文件')
+features_names = [features[i] for i in indices]
+data_ra = data1_training[[features[i] for i in indices]].copy()
+data_ra = data_ra.corr("pearson")
+pos1 = np.where(np.array((data_ra))>=0.96)[0]
+pos2 = np.where(np.array((data_ra))>=0.96)[1]
+#输出文件
+writer = pd.ExcelWriter('30个分子描述的相关性.xlsx')
+data_ra.to_excel(writer)
+writer.save()
+
+#删除10个强相关性变量
+print('开始删除10个强相关性的变量')
+delte = []
+for p in range(len(pos1)):
+    if pos1[p] < pos2[p]: 
+        delte.append(pos2[p])
+delte = list(set(delte)) #去重
+delte = np.array(delte)[0:10] #删除10个强相关性的变量
+features_names = np.delete(np.array(features_names),delte)
+print('最后对生物活性最具有显著影响的20个分子描述符为:：')
+print(features_names)
+        
 
 
